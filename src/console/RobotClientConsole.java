b232e2e1a9498098dbbd8919e4ff080d6b66448d
LostArchives <valentin.mullet@gmail.com>
2017-06-29 02:01:29 +0200

    Added sensors and traj file execution to console mode

--- src/console/RobotClientConsole.java
+++ src/console/RobotClientConsole.java
@@ -4,6 +4,8 @@ import java.util.Date;
 import java.util.Scanner;
 
 import controller.RobotClient;
+import controller.RobotIO;
+import model.RobotTrajectory;
 
 public class RobotClientConsole {
 
@@ -11,9 +13,10 @@ public class RobotClientConsole {
 	private String _userInput;
 	private Scanner _scanner;
 	private RobotClient _client;
+	private RobotTrajectory _loadedTrajectory;
 	private boolean _stop = false;
 
-	private String ascii_title = "\r\n"
+	private static String ascii_title = "\r\n"
 			+ " /$$$$$$$  /$$$$$$$   /$$$$$$         /$$$$$$  /$$ /$$                       /$$           /$$    /$$   /$$        /$$$$$$ \r\n"
 			+ "| $$__  $$| $$__  $$ /$$__  $$       /$$__  $$| $$|__/                      | $$          | $$   | $$ /$$$$       /$$$_  $$\r\n"
 			+ "| $$  \\ $$| $$  \\ $$| $$  \\__/      | $$  \\__/| $$ /$$  /$$$$$$  /$$$$$$$  /$$$$$$        | $$   | $$|_  $$      | $$$$\\ $$\r\n"
@@ -53,7 +56,30 @@ public class RobotClientConsole {
 		_userInput = "";
 		_scanner = null;
 		_client = new RobotClient();
+		_loadedTrajectory = null;
 		_stop = true;
+	}
+	
+	private void write(String text) {
+		System.out.print(text);
+	}
+
+	private void writeWithBreak(String text, int nbbreak) {
+		System.out.print(text);
+		for (int i = 0; i < nbbreak; i++) {
+			System.out.println("");
+		}
+	}
+
+	private void waitUser() {
+		_scanner = new Scanner(System.in);
+		write("> ");
+		_userInput = _scanner.nextLine();
+	}
+	
+	private void waitDrive() {
+		write(">>> ");
+		_userInput = _scanner.nextLine();
 	}
 
 	public void startConsole() {
@@ -110,8 +136,11 @@ public class RobotClientConsole {
 		case "command":
 			if (_client.is_connected()) {
 				writeWithBreak("Bienvenue dans le mode de pilotage manuel du RP6", 1);
-				writeWithBreak("Veuillez saisir une direction : f|b|l|r", 1);
+				writeWithBreak("Veuillez saisir une commande compatible avec le mode 'command'. Vous pouvez utiliser la commande .help si besoin", 1);
 				_consoleState = ConsoleState.DRIVING;
+			}
+			else {
+				writeWithBreak("Veuillez d'abord vous connecter au RP6 via la commande 'connect'", 1);
 			}
 			break;
 
@@ -171,6 +200,34 @@ public class RobotClientConsole {
 			_client.send("stp");
 			break;
 			
+		case "sensors":
+			displaySensorsForm();
+			break;
+			
+		case "exec-traj":
+			writeWithBreak("Veuillez indiquer l'emplacement du fichier .traj à exécuter", 1);
+			String traj_file = _scanner.nextLine();
+			_loadedTrajectory = RobotIO.readTrajFile(traj_file);
+			if (_loadedTrajectory != null) {
+				_loadedTrajectory.startAutoPilot();
+			}
+			else {
+				writeWithBreak("Fichier .traj valide. Veuillez vérifier que le fichier est correct.", 1);
+			}
+			
+			break;
+			
+		case "cancel-traj":
+			if (_loadedTrajectory != null && _loadedTrajectory.is_running()) {
+				_loadedTrajectory.stopAutoPilot();
+				writeWithBreak("Demande d'arrêt de l'autopilote prise en compte. Veuillez patienter quelques secondes", 1);
+			}
+			else {
+				writeWithBreak("Aucun fichier .traj en cours d'exécution", 1);
+			}
+				
+			break;
+			
 		case ".help":
 			displayHelp();
 			break;
@@ -180,16 +237,82 @@ public class RobotClientConsole {
 			writeWithBreak("Vous avez quitté le mode de pilotage", 1);
 			_consoleState = ConsoleState.RUNNING;
 			break;
+			
+		default:
+			writeWithBreak("Commande introuvable. Veuillez utiliser la commande .help si vous avez besoin d'aide", 1);
+			break;
 		}
 	}
 	
-	private void waitDrive() {
-		write(">>> ");
-		_userInput = _scanner.nextLine();
-	}
+	
 	
 	private void displaySensorsForm() {
-		writeWithBreak("", 1);
+		writeWithBreak("Veuillez indiquer l'identifiant du/des capteurs souhaités", 1);
+		waitDrive();
+		switch(_userInput) {
+			case "all":
+				writeWithBreak("Niveau de batterie : " + _client.get_data().get_batteryPower(),1);
+				writeWithBreak("Capteur de vitesse gauche : " + _client.get_data().get_speedLeft(),1);
+				writeWithBreak("Capteur de vitesse droit : " + _client.get_data().get_speedRight(),1);
+				writeWithBreak("Vitesse désirée côté gauche : " + _client.get_data().get_desiredSpeedLeft(),1);
+				writeWithBreak("Vitesse désirée côté droit : " + _client.get_data().get_desiredSpeedRight(),1);
+				writeWithBreak("Force exercée côté gauche : " + _client.get_data().get_powerLeft(),1);
+				writeWithBreak("Force exercée côté droit : " + _client.get_data().get_powerRight(),1);
+				writeWithBreak("Puissance moteur côté gauche : " + _client.get_data().get_motorCurrentLeft(),1);
+				writeWithBreak("Puissance moteur côté droit : " + _client.get_data().get_motorCurrentRight(),1);
+				writeWithBreak("Distance parcourue côté gauche : " + _client.get_data().get_distanceLeft(),1);
+				writeWithBreak("Distance parcourue côté droit : " + _client.get_data().get_distanceRight(),1);
+				writeWithBreak("Capteur de lumière côté gauchee : " + _client.get_data().get_lightSensorLeft(),1);
+				writeWithBreak("Capteur de lumière côté droit : " + _client.get_data().get_lightSensorRight(),1);
+				writeWithBreak("Distance totale parcourue (en mètres) : " + _client.get_data().getTotalDistanceMeter(),1);
+				writeWithBreak("Vitesse du robot (en cm/s) : " + _client.get_data().getSpeedPerSecond(),1);
+				break;
+			case "bp":
+				writeWithBreak("Niveau de batterie : " + _client.get_data().get_batteryPower(),1);
+				break;
+			case "lspd":
+				writeWithBreak("Capteur de vitesse gauche : " + _client.get_data().get_speedLeft(),1);
+				break;
+			case "rspd":
+				writeWithBreak("Capteur de vitesse droit : " + _client.get_data().get_speedRight(),1);
+				break;
+			case "dsl":
+				writeWithBreak("Vitesse désirée côté gauche : " + _client.get_data().get_desiredSpeedLeft(),1);
+				break;
+			case "dsr":
+				writeWithBreak("Vitesse désirée côté droit : " + _client.get_data().get_desiredSpeedRight(),1);
+				break;
+			case "pl":
+				writeWithBreak("Force exercée côté gauche : " + _client.get_data().get_powerLeft(),1);
+				break;
+			case "pr":
+				writeWithBreak("Force exercée côté droit : " + _client.get_data().get_powerRight(),1);
+				break;
+			case "mcl":
+				writeWithBreak("Puissance moteur côté gauche : " + _client.get_data().get_motorCurrentLeft(),1);
+				break;
+			case "mcr":
+				writeWithBreak("Puissance moteur côté droit : " + _client.get_data().get_motorCurrentRight(),1);
+				break;
+			case "dl":
+				writeWithBreak("Distance parcourue côté gauche : " + _client.get_data().get_distanceLeft(),1);
+				break;
+			case "dr":
+				writeWithBreak("Distance parcourue côté droit : " + _client.get_data().get_distanceRight(),1);
+				break;
+			case "lsl":
+				writeWithBreak("Capteur de lumière côté gauchee : " + _client.get_data().get_lightSensorLeft(),1);
+				break;
+			case "lsr":
+				writeWithBreak("Capteur de lumière côté droit : " + _client.get_data().get_lightSensorRight(),1);
+				break;
+			case "td":
+				writeWithBreak("Distance totale parcourue (en mètres) : " + _client.get_data().getTotalDistanceMeter(),1);
+				break;
+			case "spd":
+				writeWithBreak("Vitesse du robot (en cm/s) : " + _client.get_data().getSpeedPerSecond(),1);
+				break;
+		}
 	}
 	
 	
@@ -199,7 +322,7 @@ public class RobotClientConsole {
 		writeWithBreak("1: connect		|	Permet de se connecter au RP6", 2);
 		writeWithBreak("2: disconnect		|	Permet de se déconnecter du RP6", 2);
 		writeWithBreak("3: command		|	Permet d'entrer en mode 'command' pour contrôler le RP6", 2);
-		writeWithBreak("4: quit		|	Permet de fermer le client", 2);
+		writeWithBreak("4: quit			|	Permet de fermer le client", 2);
 		
 		writeWithBreak(
 				"------------------------------- COMMANDES FONCTIONNANT UNIQUEMENT SI L'ON EST EN MODE COMMAND  ---------------------------------------",
@@ -214,7 +337,7 @@ public class RobotClientConsole {
 				2);
 		writeWithBreak("quit			|	Permet de quitter le mode 'command'",2);
 		writeWithBreak(
-				"sensors -XXX		|	Permet d'afficher les valeurs des différents capteurs où X est l'identifiant d'un capteur",
+				"sensors			|	Permet d'afficher les valeurs des différents capteurs",
 				1);
 		writeWithBreak("			-all  : Affiche tous les capteurs", 1);
 		writeWithBreak("			-bp  :  Affiche le pourcentage restant de batterie", 1);
@@ -222,7 +345,6 @@ public class RobotClientConsole {
 		writeWithBreak("			-rspd : Capteur de vitesse droit", 1);
 		writeWithBreak("			-dsl : Vitesse désirée côté gauche", 1);
 		writeWithBreak("			-dsr : Vitesse désirée côté droit", 1);
-		writeWithBreak("			-dsl : Capteur de vitesse droit", 1);
 		writeWithBreak("			-pl :  Force exercée côté gauche", 1);
 		writeWithBreak("			-pr :  Force exercée côté droit", 1);
 		writeWithBreak("			-mcl : Puissance moteur côté gauche", 1);
@@ -238,22 +360,7 @@ public class RobotClientConsole {
 				1);
 	}
 
-	private void write(String text) {
-		System.out.print(text);
-	}
-
-	private void writeWithBreak(String text, int nbbreak) {
-		System.out.print(text);
-		for (int i = 0; i < nbbreak; i++) {
-			System.out.println("");
-		}
-	}
-
-	private void waitUser() {
-		_scanner = new Scanner(System.in);
-		write("> ");
-		_userInput = _scanner.nextLine();
-	}
+	
 	
 	private void displayConnectForm() {
 		String adr_ip = "";
@@ -271,7 +378,7 @@ public class RobotClientConsole {
 				while(_client.is_connecting()) {
 					write(".");
 					try {
-						Thread.sleep(200);
+						Thread.sleep(100);
 					} catch (InterruptedException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
@@ -290,5 +397,6 @@ public class RobotClientConsole {
 		t1.start();
 		
 	}
+	
 
 }
